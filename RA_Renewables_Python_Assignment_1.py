df76842ed6f08e8eebd7022fdc15008576281911
Declan Ryan <DeclanRyan@Aislings-MacBook-Pro.local>
2015-10-11 21:53:28 +0100

    Final commit program complete.

--- RA_Renewables_Python_Assignment_1.py
+++ RA_Renewables_Python_Assignment_1.py
@@ -12,110 +12,68 @@ conn = sqlite3.connect('renewable.db')
 c = conn.cursor()
 c.execute("SELECT long FROM location")
 longitude = []
-print "Longitude coordinates for all 10 Plant locations:"
 for item in c:
     print item 
     longitude.append(item)    
-print
-print "Plant 1's longitude coordinate is: ", longitude[0]
-print
 
 #Import and create list for Plant location latitudes
 c.execute("SELECT lat FROM location")
 latitude = []
-print "Latitude coordinates for all 10 Plant locations:"
 for item in c:
     print item 
     latitude.append(item)    
-print
-print "Plant 1's Latitude coordinate is: ", latitude[0]
-print
 
 #Import and create list for each Plant's production in tonnes
 conn = sqlite3.connect('renewable.db')
 c = conn.cursor()
 c.execute("SELECT production FROM location")
 production = []
-print "Productiion numbers for all 10 Plant locations:"
 for item in c:
     print item 
     production.append(item)    
-print
-print "Plant 1's Production is: ", production[0], " tonnes."
-print
 
 #Import and create list for Port location longitudes
 conn = sqlite3.connect('renewable.db')
 c = conn.cursor()
 c.execute("SELECT long FROM ports")
 portLongitude = []
-print "Longitude coordinates for all 3 Port locations:"
 for item in c:
     print item 
     portLongitude.append(item)    
-print "Port 3's Longitude coordinate is: ", portLongitude[2]
-print
 
 #Import and create list for Port location latitudes
 conn = sqlite3.connect('renewable.db')
 c = conn.cursor()
 c.execute("SELECT lat FROM ports")
 portLatitude = []
-print "Latitude coordinates for all 3 Port locations:"
 for item in c:
     print item 
     portLatitude.append(item)    
-print
-print "Port 3's Latitude coordinate is:  ", portLatitude[2]
-print
-
-print "GitHub Test"
-print "Why won't GitHub recognize that I've made changes to this file"
 
 # This is a function to calculate the distance between 2 points
-# that I found on the Internet
+# given the longitude and latitude of the two points
 def distance(lat1, long1, lat2, long2):
- 
-    # Convert latitude and longitude to 
-    # spherical coordinates in radians.
-    degrees_to_radians = math.pi/180.0
-         
-    # phi = 90 - latitude
+# Convert latitude and longitude to spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0     
+# phi = 90 - latitude
     phi1 = (90.0 - lat1)*degrees_to_radians
-    phi2 = (90.0 - lat2)*degrees_to_radians
-         
-    # theta = longitude
+    phi2 = (90.0 - lat2)*degrees_to_radians   
+# theta = longitude
     theta1 = long1*degrees_to_radians
     theta2 = long2*degrees_to_radians
-         
-    # Compute spherical distance from spherical coordinates.
-         
-    # For two locations in spherical coordinates 
-    # (1, theta, phi) and (1, theta', phi')
-    # cosine( arc length ) = 
-    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
-    # distance = rho * arc length
-     
-    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
-           math.cos(phi1)*math.cos(phi2))
-    arc = math.acos( cos )
- 
-    # Remember to multiply arc by the radius of the earth 
-    # in your favorite set of units to get length.
+#Calculate spherical distance from spherical coordinates for two locations 
+#in spherical coordinates (1, theta, phi) and (2, theta', phi')
+#cosine(arc length) = sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+#distance = rho * arc length
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + math.cos(phi1)*math.cos(phi2))
+    arc = math.acos(cos)
+#Units are not important as we are only trying to find the 
+#best option relative to all the others
     return arc
-#I don't think I need this function
-#Function to determine which port is best for each location
-'''def cheapestPortFunction(List):
-    n = 1
-    bestOption = (cheapestPort[n-1].index) + 1 '''Representing the first port'''
-    while n < 3:    
-        if cheapestPort[n] < cheapestPort[n-1]:
-            bestOption = (cheapestPort[n].index) + 1
-        n += 1
-    return bestOption
-'''
 
-#Loops within a loop to call the distance function 
+#Loops within a loop to call the distance function and calculate
+#the best option re. distance between locations, production and
+#the distance to the 3 ports
 i = 0
 k = 0
 p = 0
@@ -131,30 +89,28 @@ while i < 10:
         totalCost = temp * distance(longitude[i], latitude[i], portLongitude[k], portLatitude[k])
         portCostList[p] = totalCost  '''Creates a list of 30 floating point numbers representing each of 3 ports for each location'''
         p += 1
-#    cheapestPort = cheapestPortFunction(portCostList)
     i+=1
-'''We now have a list called portCostList with 30 entries
-so we need to find the lowest number in this list find it's index
-and determine which location and which port is associated with this
-value based on the fact that indexes 0, 1 and 2 are from 
-ports 1, 2 and 3 with respect to location 1 etc.  '''
+#We now have a list called portCostList with 30 entries
+#so we need to find the lowest number in this list, find it's index
+#and determine which location and which port is associated with this
+#value based on the fact that indexes 0, 1 and 2 are from 
+#ports 1, 2 and 3 with respect to location 1 etc.  '''
 
-k = 0
-bestOption = 0
-while k < 30;:
-    if portCostList[k + 1] < portCostList[k]:
-        bestOption = k
-    k += 1
+m = 0 '''Set counter variable m to 0 for first entry in portCostList'''
+bestOption = 0  '''Sets first entry as the initial best option'''
+while m < 30;
+    if portCostList[m + 1] < portCostList[m]:
+        bestOption = m + 1
+    m += 1
 #We need to add one to bestOption because the list index starts at 0
-bestOption =bestOption +1    
+bestOption += 1    
 print "The best option is number " bestOption, " of 30"
-#Determine which location and which port this option gives us
 
+#Determine which location and which port this option corresponds to
 locationNumber = (bestOption/3) + 1 
-'''To take into account that 3 will go into bestOption 
-one less than the location number it refers to'''
+#To take into account that 3 will go into bestOption one less
+#than the location number it refers to
 portNumber = bestOption % 3
-
-print "The very best option is to set up the plant "
-print "at location number ", locationNumber, " and "
-print "transport the products from port number ", portNumber
+print "The very best option is to set up the production plant "
+print "at location number ", locationNumber, " and transport "
+print "the products from port number ", portNumber, "."
